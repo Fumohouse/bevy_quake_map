09b83cefefc2a6d4de293def2bf497d070285458
kyoseki <76502906+kyoseki@users.noreply.github.com>
2022-06-04 10:17:04 -0700

    Crude hot reloading support

--- src/loader/spawner.rs
+++ src/loader/spawner.rs
@@ -1,8 +1,15 @@
 //! Based on bevy_scene scene_spawner
 
 use super::asset::MapAsset;
-use bevy::prelude::*;
+use bevy::{
+    ecs::event::{Events, ManualEventReader},
+    hierarchy::BuildWorldChildren,
+    pbr::PbrBundle,
+    prelude::{default, AssetEvent, Assets, Entity, Handle, Mut, Transform, World},
+    transform::TransformBundle,
+};
 use heron::RigidBody;
+use std::collections::HashMap;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -14,6 +21,9 @@ pub enum MapSpawnError {
 #[derive(Default)]
 pub struct MapSpawner {
     queued_maps: Vec<Handle<MapAsset>>,
+    queued_for_respawn: Vec<Handle<MapAsset>>,
+    event_reader: ManualEventReader<AssetEvent<MapAsset>>,
+    entities: HashMap<Handle<MapAsset>, Entity>,
 }
 
 impl MapSpawner {
@@ -21,38 +31,45 @@ impl MapSpawner {
         self.queued_maps.push(handle);
     }
 
-    fn spawn_handle(world: &mut World, handle: Handle<MapAsset>) -> Result<(), MapSpawnError> {
+    fn spawn_handle(&mut self, world: &mut World, handle: Handle<MapAsset>) -> Result<(), MapSpawnError> {
         world.resource_scope(|world, assets: Mut<Assets<MapAsset>>| {
             let map = assets
-                .get(handle)
+                .get(handle.clone_weak())
                 .ok_or_else(|| MapSpawnError::MapNotLoaded)?;
 
-            for entity in &map.entities {
-                let mut ecs_entity = world.spawn();
-                ecs_entity.insert_bundle(TransformBundle::identity());
+            let mut map_entity = world.spawn();
+            map_entity.insert_bundle(TransformBundle::identity());
 
-                ecs_entity.with_children(|parent| {
-                    for brush in &entity.brushes {
-                        let mut ecs_brush = parent.spawn_bundle(TransformBundle::from_transform(
-                            Transform::from_translation(brush.position),
-                        ));
+            map_entity.with_children(|parent| {
+                let mut ecs_entity = parent.spawn_bundle(TransformBundle::identity());
 
-                        ecs_brush
-                            .insert(RigidBody::Static)
-                            .insert(brush.collider.clone());
+                for entity in &map.entities {
+                    ecs_entity.with_children(|parent| {
+                        for brush in &entity.brushes {
+                            let mut ecs_brush =
+                                parent.spawn_bundle(TransformBundle::from_transform(
+                                    Transform::from_translation(brush.position),
+                                ));
 
-                        ecs_brush.with_children(|parent| {
-                            for (_tex_name, (mesh_handle, mat_handle)) in &brush.meshes {
-                                parent.spawn_bundle(PbrBundle {
-                                    mesh: mesh_handle.clone(),
-                                    material: mat_handle.clone(),
-                                    ..default()
-                                });
-                            }
-                        });
-                    }
-                });
-            }
+                            ecs_brush
+                                .insert(RigidBody::Static)
+                                .insert(brush.collider.clone());
+
+                            ecs_brush.with_children(|parent| {
+                                for (_tex_name, (mesh_handle, mat_handle)) in &brush.meshes {
+                                    parent.spawn_bundle(PbrBundle {
+                                        mesh: mesh_handle.clone(),
+                                        material: mat_handle.clone(),
+                                        ..default()
+                                    });
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+
+            self.entities.insert(handle.clone_weak(), map_entity.id());
 
             Ok(())
         })
@@ -62,7 +79,7 @@ impl MapSpawner {
         let queued = std::mem::take(&mut self.queued_maps);
 
         for handle in queued {
-            match Self::spawn_handle(world, handle.clone()) {
+            match self.spawn_handle(world, handle.clone()) {
                 Ok(_) => {}
                 Err(MapSpawnError::MapNotLoaded) => self.spawn(handle),
                 Err(e) => return Err(e),
@@ -74,6 +91,24 @@ impl MapSpawner {
 
     pub fn system(world: &mut World) {
         world.resource_scope(|world, mut map_spawner: Mut<MapSpawner>| {
+            let map_asset_events = world.resource::<Events<AssetEvent<MapAsset>>>();
+
+            let mut to_respawn = Vec::new();
+
+            for event in map_spawner.event_reader.iter(map_asset_events) {
+                if let AssetEvent::Modified { handle } = event {
+                    to_respawn.push(handle.clone_weak());
+                }
+            }
+
+            for handle in to_respawn {
+                if let Some(entity) = map_spawner.entities.remove(&handle) {
+                    world.despawn(entity);
+                }
+
+                map_spawner.queued_maps.push(handle);
+            }
+
             map_spawner.spawn_queued(world).unwrap_or_else(|e| {
                 panic!("map spawn failed: {}", e);
             });
